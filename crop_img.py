32e1af406a2dbbe52e6a11222c448968205644fe
co2e14 <christian.orr@diamond.ac.uk>
2022-07-16 11:33:13 +0100

    update

---
+++ crop_img.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+path = os.getcwd()
+croppedpath = os.path.join(path, "cropped")
+
+def showimg(toshow):    
+    cv2.imshow("image", toshow)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def croppit(filein):
+    img = cv2.imread(filein)
+    cropped_image = img[400:650, 550:800]
+    outfile = os.path.join(croppedpath, os.path.basename(filein))
+    print(outfile)
+    cv2.imwrite(outfile, cropped_image)
+    
+dir = input("Path to files: ")
+dir = os.path.join(path, dir)
+print(dir)
+for file in os.listdir(dir):
+    if file.endswith("jpg"):
+        print(file)
+        file = os.path.join(dir, file)
+        croppit(file)
+
---
+++ install.py
@@ -0,0 +1,36 @@
+import os
+
+paths = {
+    'WORKSPACE_PATH': os.path.join('Tensorflow', 'workspace'),
+    'SCRIPTS_PATH': os.path.join('Tensorflow','scripts'),
+    'APIMODEL_PATH': os.path.join('Tensorflow','models'),
+    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations'),
+    'IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images'),
+    'MODEL_PATH': os.path.join('Tensorflow', 'workspace','models'),
+    'PRETRAINED_MODEL_PATH': os.path.join('Tensorflow', 'workspace','pre-trained-models'),
+    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
+    'OUTPUT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
+    'TFJS_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
+    'TFLITE_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
+    'PROTOC_PATH':os.path.join('Tensorflow','protoc')
+ }
+ files = {
+    'PIPELINE_CONFIG':os.path.join('Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    'TF_RECORD_SCRIPT': os.path.join(paths['SCRIPTS_PATH'], TF_RECORD_SCRIPT_NAME), 
+    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
+}
+
+for path in paths.values():
+    if not os.path.exists(path):
+        if os.name == 'posix':
+            !mkdir -p {path}
+        if os.name == 'nt':
+            !mkdir {path}
+
+if not os.path.exists(os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection')):
+    !git clone https://github.com/tensorflow/models {paths['APIMODEL_PATH']}
+
+if os.name=='posix':  
+    !apt-get install protobuf-compiler
+    !cd Tensorflow/models/research && protoc object_detection/protos/*.proto --python_out=. && cp object_detection/packages/tf2/setup.py . && python -m pip install . 
+
