06f9170c760ab8497d4d052f00e2641838f1b723
co2e14 <christian.orr@diamond.ac.uk>
2025-02-17 13:23:42 +0000

    add inference script for binary classification with accuracy evaluation

---
+++ GoniOwl_infer.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import os
+from tensorflow import keras
+
+
+model = tf.keras.models.load_model("20250215-200544_save_binary_batch16.h5")
+model.summary()
+classes = ["pinoff", "pinon"]
+test_data_dir = "test_14022025_binary"
+
+def infer(image, class_):
+    img_in = keras.preprocessing.image.load_img(image, target_size=(800, 800))
+    img_array = keras.preprocessing.image.img_to_array(img_in)
+    img_array = tf.expand_dims(img_array, 0)
+    predictions = model.predict(img_array, verbose=0)
+    score = predictions[0]
+    if score < 0.5:
+        print(f"{score} is {classes[0]}")
+        predicted_label = classes[0]
+    else:
+        print(f"{score} is {classes[1]}")
+        predicted_label = classes[1]
+
+    if predicted_label == class_: 
+        print(f"Correctly classified {image} as {predicted_label}")
+        return 1
+    else:
+        print(f"Incorrectly classified {image} as {predicted_label}")
+        return 0
+    
+if __name__ == "__main__":
+    correct = 0
+    total = 0
+    for class_ in classes:
+        class_dir = os.path.join(test_data_dir, class_)
+        for image_name in os.listdir(class_dir):
+            image_path = os.path.join(test_data_dir, class_, image_name)
+            correct += infer(image_path, class_)
+            total += 1
+    
+    accuracy = correct / total
+    print(f"Accuracy: {accuracy * 100:.2f}%")
