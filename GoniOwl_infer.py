564160f3588f081e432ec3988ea55fe743520dda
co2e14_home <christian.orr@diamod.ac.uk>
2025-07-23 15:51:42 +0100

    add dynamic image size adjustment for inference

--- GoniOwl_infer.py
+++ GoniOwl_infer.py
@@ -8,9 +8,23 @@ model.summary()
 classes = ["pinoff", "pinon"]
 test_data_dir = "test_19022025_binary"
 
+def get_first_image_size(data_dir):
+    pinon_dir = os.path.join(data_dir, "pinon")
+    files = [
+        f for f in os.listdir(pinon_dir) if os.path.isfile(os.path.join(pinon_dir, f))
+    ]
+    if not files:
+        raise FileNotFoundError("No files found in pinon directory")
+    first_file = os.path.join(pinon_dir, files[0])
+    img_bytes = tf.io.read_file(first_file)
+    img = tf.image.decode_image(img_bytes)
+    height = img.shape[0]
+    width = img.shape[1]
+    return height, width
 
 def infer(image, class_):
-    img_in = keras.preprocessing.image.load_img(image, target_size=(800, 800))
+    h, w = get_first_image_size(test_data_dir)
+    img_in = keras.preprocessing.image.load_img(image, target_size=(int(h), int(w)))
     img_array = keras.preprocessing.image.img_to_array(img_in)
     img_array = tf.expand_dims(img_array, 0)
     predictions = model.predict(img_array, verbose=0)
